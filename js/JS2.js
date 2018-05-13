15a6f97d7b734f0cd8a51c8c094780e6233906a2
YiKang <2032049405@qq.com>
2018-05-13 17:47:47 +0800

    任务5

--- js/JS2.js
+++ js/JS2.js
@@ -1,42 +1,84 @@
-var importNum = document.getElementById('import');
+var entranceNum = document.getElementById('entrance');
 var slideNum = document.getElementById('slide');
 
 slideNum.onchange = function () {
-    importNum.value = slideNum.value;
+    entranceNum.value = slideNum.value;
 };
 
-importNum.onchange = function () {
-    if (importNum.value >3 && importNum.value<19){
-        importNum.value = slideNum.value;
+entranceNum.onchange = function () {
+    if (entranceNum.value >3 && entranceNum.value<19){
+        entranceNum.value = slideNum.value;
     }else{
         alert('请输入正确的人数')
     }
 };
 var left = document.getElementById('left');
 left.onclick = function () {
-    if (importNum.value > 4){
-        importNum.value--;
-        slideNum.value = importNum.value;
+    if (entranceNum.value > 4){
+        entranceNum.value--;
+        slideNum.value = entranceNum.value;
     }  else {
-        alert('人太少了多找几个人来吧')
+        alert('人太少了');
     }
 };
 var right = document.getElementById('right');
 right.onclick = function () {
-    if (importNum.value < 18){
-        importNum.value++;
-        slideNum.value = importNum.value;
+    if (entranceNum.value < 18){
+        entranceNum.value++;
+        slideNum.value = entranceNum.value;
     }  else {
-        alert('人太多了')
+        alert('人太多了');
     }
 };
-document.getElementsByClassName('go').addEventListener('click',function () {
-    if (importNum.value!=document.getElementsByClassName('li').length){
-        alert('请分配好人数');
-    }
-    else{
-        window.location.href='./task5.html'
+document.getElementsByClassName('go')[0].addEventListener('click',function () {
+    if (document.getElementsByClassName('add-flat').length<2){
+        alert('请分配好人数')
+    }else{
+        window.location.href='./JS3.html'
     }
 });
 
 
+var slayer="<li class='add-slayer'>殺手1人</li>";
+
+var rolm="<li class='add-flat'>水民1人</li>";
+
+setting.onclick = function () {
+    var entrance = [];
+      var str ="";
+    entrance.length = entranceNum.value;
+    var slayer = Math.floor(entranceNum.value/3);
+var out=[];
+while(out.length<slayer){
+    var stochastic= Math.floor(Math.random()*entranceNum.value);
+    if(out.indexOf(stochastic)<0){
+        out.push(stochastic);
+    }
+    // console.log(out);
+}
+ for (i =0; i<out.length; i++){
+     entrance[out[i]] ='<li class="add-slayer">'+'杀手'+'</li>';
+     // console.log(i);
+     // console.log(out)
+     // console.log(entrance)
+}
+for (i=0; i<entrance.length; i++){
+    if(entrance[i] !=='<li class="add-slayer">' + '杀手' + '</li>'){
+        entrance[i] ='<li class="add-flat">'+'平民'+'</li>';
+    }
+    // console.log(entrance.length)
+    // console.log(i)
+}
+
+console.log(entrance);
+for (var i=0;i<entrance.length;i++){
+     str +=entrance[i];
+
+}
+
+
+
+ document.getElementById('info').innerHTML = str;
+sessionStorage.setItem('dog',JSON.stringify(entrance))
+};
+console.log(entrance);
--- js/task1.js
+++ js/task1.js
@@ -1,150 +1,47 @@
-// 底部滑动条
-
-var playerNum = document.getElementById("player");
-console.log(playerNum);
-var rangeNum = document.getElementById("myRange");
-console.log(rangeNum);
-
-// 绑定更替滑动条的值和input的值
-rangeNum.onchange = function () {
-    playerNum.value = rangeNum.value;
-}
-//  需要添加实时改变，让input的值显示到滑动条上
-playerNum.onchange = function () {
-    if (playerNum.value > 3 && playerNum.value < 19) {
-        rangeNum.value = playerNum.value;
-    } else {
-        alert("请输入正确的值位于4-18之间")
-    }
-}
-
-// 绑定加事件
-var add = document.getElementById("add");
-add.onclick = function () {
-    if (playerNum.value < 18) {
-        playerNum.value++;
-        rangeNum.value = playerNum.value;
-    } else {
-        alert("人够多了，等下一局吧！");
-    }
-}
-
-// 绑定减事件
-var less = document.getElementById("less");
-less.onclick = function () {
-    if (playerNum.value > 4) {
-        playerNum.value--;
-        rangeNum.value = playerNum.value;
-    } else {
-        alert("人不够，快来人啦！");
-    }
-}
-// 限制必须先设置好人数才能进入页面三
-document.getElementsByClassName("goto")[0].addEventListener("click", function()
-{
-    if(playerNum.value!=document.getElementsByTagName("li").length){
-        alert("玩家人数还没设置好");
-    }
-    else{
-        window.location.href = './task3-1.html';
-    }
-});
-// sort()
-// 设置本地存储
-localStorage.setItem("lastname", "Gates");
-
-var killer = '<li class="pfs-killer">杀手1人</li>';
-var civilian = '<li class="pfs-civilian">平民1人</li>';
-
-// setting的事件
-setting.onclick = function () {
-
-    // // 洗牌算法
-    // // 先隐藏ul标签中的内容
-    // info.innerHTML = "";
-    // // 确定 杀手和平民的个数
-    // var player = [];
-    // var killers = [];
-    // var civilians = [];
-    // killers.length = parseInt(playerNum.value / 3);
-    // civilians.length = playerNum.value - killers.length;
-
-    // console.log(killers.length);
-    // console.log(civilians.length);
-
-    // //将killers的每一项都设置为杀手元素
-    // for (var i = 0; i < killers.length; i++) {
-    //   killers[i] = killer;
-    // }
-    // //将civilians的每一项都设置为平民元素
-    // for (var j = 0; j < civilians.length; j++) {
-    //   civilians[j] = civilian;
-    // }
-    // // console.log(civilians);
-    // // console.log(killers);
-
-    // // 合并到总人数
-    // player = killers.concat(civilians);
-    // // console.log(player);
-    // var x = player.length;
-    // // console.log(player.length);
-    // var out = [];
-    // for (a = 0; a < x; a++) {
-    //   var index = Math.floor(Math.random() * player.length);
-    //   // console.log(player.length);
-    //   // console.log(index);
-    //   out[a] = player[index];
-    //   player.splice(index, 1);
-    // }
-    // console.log(out);
-
-    // // 传入数据
-    // for (b = 0; b < out.length; b++) {
-    //   info.innerHTML += out[b];
-    // }
+// 点击次数
+var time = 1;
+// 上面是数字
+var num = 1;
+var obj = JSON.stringify(player);
+obj = sessionStorage.player;
+var player = JSON.parse(obj);
 
 
-    // 自己的想法
-    // 所有的人数数组
-
-    var player = [];
-    var str = "";
-    player.length = playerNum.value;
-    var killer = Math.floor(playerNum.value / 3);
-
-    // 杀手人数随机定位
-    var out = [];
-    while (out.length < killer) {
-        var randomindex = Math.floor(Math.random() * playerNum.value);
-        if (out.indexOf(randomindex) < 0) {
-            out.push(randomindex);
+// 初始状态通用
+$('.number').text(num);
+// 点击事件
+$("#look").click(function () {
+    // $("#pass,#show").toggle();
+    // 点击次数奇数次
+    if (time % 2 == 1) {
+        $("#pass").show();
+        $("#show").hide();
+        var sub = player[num];
+        num++;
+        if (num > player.length) {
+            $('#look').text("法官查看");
+        } else {
+            sub = (sub.substring(sub.length - 7, sub.length - 5));
+            // console.log(sub);
+            $('#who').text(sub);
+            $('#look').text("隐藏身份并传递给 " + (num) + " 号");
         }
-        // console.log(out);
+        console.log(num);
     }
-    // 给杀手传参
-    for (i = 0; i < out.length; i++) {
-        // 注意这里的out[i]就是定义位置了
-        player[out[i]] = '<li class="killer">' + '杀手' + '</li>';
-    }
-
-
-    for (i = 0; i < player.length; i++) {
-        if (player[i] !== '<li class="killer">' + '杀手' + '</li>') {
-            player[i] = '<li class="civilian">' + '平民' + '</li>';
+    // 点击次数偶数次
+    else {
+        if ($('#look').text() == "法官查看") {
+            window.location.href = 'task3-2.html';
+        } else {
+            $("#pass").hide();
+            $("#show").show();
+            if (num > player.length) {
+                window.location.href = 'task3-2.html';
+            } else {
+                $('#look').text("查看 " + (num) + "号身份");
+                $('.circle').text(num);
+            }
         }
     }
-    // console.log(player);
-
-    for (var i = 0; i < player.length; i++) {
-        str += player[i];
-    }
-    // console.log(str);
-
-    document.getElementById("info").innerHTML = str;
-
-    var obj = JSON.stringify(player);
-    // console.log(obj);
-    // 存入
-    sessionStorage.player = obj;
-    // 用sessionStorage实现页面之间的数据传输                                                                                                                                                                     ,m
-}
+    time++;
+})
